12a2846699c3c256cfed19a62c5dd34dde05e585
evilguy50 <robbryan42@gmail.com>
2021-09-04 20:13:26 -0700

    Update shorthand.py

--- shorthand/shorthand.py
+++ shorthand/shorthand.py
@@ -1,3 +1,22 @@
-import lib.shorthand as shorthand
+import configparser
+from os import remove
+import glob
 
-shorthand.short()
+config = configparser.ConfigParser()
+config.read('BP/filters/shorthand/config.ini')
+shorts = config["define"]["shorts"]
+pshort = shorts.split()
+for filename in glob.iglob("./BP/functions/" + '**/*.mcfunction', recursive=True):
+    print(filename)
+    ropen = open(filename, "r")
+    rFile = ropen.read()
+    ropen.close()
+    for i in pshort:
+        short = i
+        long = config["as"]["{}_long".format(i)]
+        print("short = {arg1}, long = {arg2}".format(arg1 = short, arg2 = long))
+        rFile = rFile.replace(short, long)
+    remove(filename)
+    newopen = open(filename, "x")
+    newopen.write(rFile)
+    newopen.close()
