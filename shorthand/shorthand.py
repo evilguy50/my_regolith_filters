51cf33daf617fd9204c8a75419d84a0368e783ae
evilguy50 <robbryan42@gmail.com>
2021-10-11 20:41:33 -0700

    Update shorthand.py

--- shorthand/shorthand.py
+++ shorthand/shorthand.py
@@ -3,9 +3,9 @@ import os
 import glob
 
 config = configparser.ConfigParser()
-if not os.path.isDir("data/shorthand"):
+if not os.path.isdir("data/shorthand"):
     os.mkdir("data/shorthand")
-if not os.path.isFile("data/shorthand/config.ini"):
+if not os.path.isfile("data/shorthand/config.ini"):
     f = os.open("data/shorthand/config.ini", "x")
     os.write(f, "[define]\nshorts = admin armors\n[as]\nadmin_long = @a[tag=admin]\narmors_long = @e[type=armor_stand]")
 config.read('BP/filters/shorthand/config.ini')
